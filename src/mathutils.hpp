1164004bfcb55f08d1428e3d49c02ef0a66be8ae
Iyad Ahmed <iyadahmed@cgonfire.com>
2022-02-26 23:08:34 +0200

    Update mathutils

--- src/mathutils.hpp
+++ src/mathutils.hpp
@@ -1,6 +1,9 @@
 #ifndef MATHUTILS_HPP
 #define MATHUTILS_HPP
 
+#include <assert.h>
+#include <iostream>
+#include <math.h>
 #include <stdint.h>
 
 inline void sub_v3v3(float out[3], const float a[3], const float b[3]) {
@@ -35,7 +38,7 @@ inline void scale_v3(float out[3], float v[3], float scale) {
 
 inline float dot_v3v3(const float a[3], const float b[3]) { return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]; }
 
-inline void cross_v3v3(float out[3], float a[3], float b[3]) {
+inline void cross_v3v3(float out[3], const float a[3], const float b[3]) {
   out[0] = a[1] * b[2] - a[2] * b[1];
   out[1] = a[2] * b[0] - a[0] * b[2];
   out[2] = a[0] * b[1] - a[1] * b[0];
@@ -47,6 +50,8 @@ inline void copy_v3v3(float out[3], const float v[3]) {
   out[2] = v[2];
 }
 
+inline float length_sq_v3(const float v[3]) { return dot_v3v3(v, v); }
+
 inline float length_v3(const float v[3]) { return sqrtf(dot_v3v3(v, v)); }
 
 inline void normalize_v3v3(float out[3], const float v[3]) {
@@ -54,6 +59,15 @@ inline void normalize_v3v3(float out[3], const float v[3]) {
   float l = length_v3(v);
   if (fabsf(l) > 1e-5f) {
     scale_v3(out, out, 1.0f / l);
+  }
+}
+
+inline void normalize_v3v3(float v[3]) {
+  float l = length_v3(v);
+  if (fabsf(l) > 1e-5f) {
+    v[0] /= l;
+    v[1] /= l;
+    v[2] /= l;
   }
 }
 
@@ -90,7 +104,9 @@ struct Plane {
 typedef Plane Line;
 
 // https://en.wikipedia.org/w/index.php?title=Line%E2%80%93plane_intersection&oldid=1030561834
-inline void line_plane_intersection(LineIntersectionResult *out, Plane *plane, Line *line) {
+inline void line_plane_intersection(LineIntersectionResult *out, const Plane *plane, const Line *line) {
+  assert(fabsf(length_sq_v3(line->direction) - 1.0f) <= 1e-5f);
+  assert(fabsf(length_sq_v3(plane->direction) - 1.0f) <= 1e-5f);
 
   float d_numerator = ((plane->co[0] - line->co[0]) * plane->direction[0]) +
                       ((plane->co[1] - line->co[1]) * plane->direction[1]) +
@@ -112,6 +128,42 @@ inline void line_plane_intersection(LineIntersectionResult *out, Plane *plane, L
     out->single_intersection_point[1] = line->co[1] + line->direction[1] * d;
     out->single_intersection_point[2] = line->co[2] + line->direction[2] * d;
   }
+}
+
+inline void line_line_intersection(LineIntersectionResult *out, const Line *l1, const Line *l2) {
+  assert(fabsf(length_sq_v3(l1->direction) - 1.0f) <= 1e-5f);
+  assert(fabsf(length_sq_v3(l2->direction) - 1.0f) <= 1e-5f);
+
+  float shared_plane_direction[3];
+  float l2_l1[3];
+  float l2_l1_cross_dir[3];
+
+  cross_v3v3(shared_plane_direction, l1->direction, l2->direction);
+  sub_v3v3(l2_l1, l2->co, l1->co);
+  cross_v3v3(l2_l1_cross_dir, l2_l1, l1->direction);
+
+  if (length_sq_v3(shared_plane_direction) <= 1e-5f) {
+    // Lines are parallel
+    if (length_sq_v3(l2_l1_cross_dir) <= 1e-5f) {
+      // Lines are collinear
+      out->type = LineIntersectionType::CONTAINED;
+    } else {
+      // Lines don't intersect
+      out->type = LineIntersectionType::NONE;
+    }
+    return;
+  }
+
+  if (fabsf(dot_v3v3(l2_l1, shared_plane_direction)) <= 1e-5f) {
+    // Lines are not in same plane ("Skew Lines")
+    out->type = LineIntersectionType::NONE;
+    return;
+  }
+
+  Plane l2_plane;
+  copy_v3v3(l2_plane.co, l1->co);
+  copy_v3v3(l2_plane.direction, l2->direction);
+  line_plane_intersection(out, &l2_plane, l2);
 }
 
 #endif
--- src/test_mathutils.cpp
+++ src/test_mathutils.cpp
@@ -6,10 +6,15 @@
 
 #include "mathutils.hpp"
 
+// Use real data instead of pointless iteration, as compiler might optimize away the iteration
+
 void test_line_plane_intersection() {
-  LineIntersectionResult res{LineIntersectionType::NONE, {0.0f}};
-  Plane plane{{1.0f, 1.0f, 1.0f}, {1.0f, 1.0f, 1.0f}};
-  Line line{{0.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 1.0f}};
+  LineIntersectionResult res{LineIntersectionType::NONE, {0.0f, 0.0f, 0.0f}};
+  float plane_normal[3] = {1.0f, 1.0f, 1.0f};
+  normalize_v3v3(plane_normal);
+
+  Plane plane{{1.0f, 1.0f, 1.0f}, {plane_normal[0], plane_normal[1], plane_normal[2]}};
+  Line line{{0.0f, 0.0f, 0.0f}, {plane_normal[0], plane_normal[1], plane_normal[2]}};
 
   for (uint32_t i = 0; i < 10'000'000; i++) {
     line_plane_intersection(&res, &plane, &line);
@@ -31,8 +36,19 @@ void test_cross_product() {
   assert(fabs(out[2] - 1.0f) <= 1e-5f);
 }
 
+void test_line_line_intersect() {
+  LineIntersectionResult res{LineIntersectionType::NONE, {0.0f, 0.0f, 0.0f}};
+  Line l1{{0.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f}};
+  Line l2{{-1.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}};
+
+  for (uint32_t i = 0; i < 10'000'00; i++) {
+    line_line_intersection(&res, &l1, &l2);
+  }
+}
+
 int main() {
   test_line_plane_intersection();
   test_cross_product();
+  test_line_line_intersect();
   return 0;
 }
