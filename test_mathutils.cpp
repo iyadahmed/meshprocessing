5c86d4335bd744250adecc9bd1775fc7dd240b1d
Iyad Ahmed <iyadahmed@cgonfire.com>
2022-02-26 13:35:00 +0200

    Update test_mathutils

--- test_mathutils.cpp
+++ test_mathutils.cpp
@@ -1,3 +1,24 @@
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+
 #include "mathutils.hpp"
 
-int main(void) { return 0; }
+int main(void) {
+  float pco[3] = {1.0f, 1.0f, 1.0f};
+  float pno[3] = {1.0f, 1.0f, 1.0f};
+  float lco[3] = {0.0f, 0.0f, 0.0f};
+  float ldir[3] = {1.0f, 1.0f, 1.0f};
+  LinePlaneIntersectionResult res{LinePlaneIntersectionType::NONE, {0.0f}};
+
+  for (uint32_t i = 0; i < 1000'000'0; i++) {
+    line_plane_intersection(&res, pco, pno, lco, ldir);
+  }
+
+  assert(res.type == LinePlaneIntersectionType::SINGLE_POINT);
+  assert(res.single_intersection_point[0] == 1.0f);
+  assert(res.single_intersection_point[1] == 1.0f);
+  assert(res.single_intersection_point[2] == 1.0f);
+  return 0;
+}
