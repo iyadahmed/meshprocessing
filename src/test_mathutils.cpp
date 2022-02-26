6ed29f0945f7efa30eaa57a7556d194c89a21b3f
Iyad Ahmed <iyadahmed@cgonfire.com>
2022-02-26 16:43:23 +0200

    Update test_mathutils

--- src/test_mathutils.cpp
+++ src/test_mathutils.cpp
@@ -2,10 +2,11 @@
 #include <cmath>
 #include <cstdint>
 #include <iostream>
+#include <random>
 
 #include "mathutils.hpp"
 
-int main(void) {
+void test_line_plane_intersection() {
   float pco[3] = {1.0f, 1.0f, 1.0f};
   float pno[3] = {1.0f, 1.0f, 1.0f};
   float lco[3] = {0.0f, 0.0f, 0.0f};
@@ -20,5 +21,18 @@ int main(void) {
   assert(res.single_intersection_point[0] == 1.0f);
   assert(res.single_intersection_point[1] == 1.0f);
   assert(res.single_intersection_point[2] == 1.0f);
+}
+
+void test_cross_product() {
+  float out[3]{};
+  float a[3] = {1.0f, 0.0f, 0.0f};
+  float b[3] = {0.0f, 1.0f, 0.0};
+  cross_v3v3(out, a, b);
+  assert(fabs(out[2] - 1.0f) <= 1e-5f);
+}
+
+int main() {
+  test_line_plane_intersection();
+  test_cross_product();
   return 0;
 }
