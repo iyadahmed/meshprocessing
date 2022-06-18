bb1e2b5bb8a143af275b5981e6b2bf4226c59e5c
Iyad Ahmed <iyadahmed430@gmail.com>
2022-06-18 19:50:45 +0200

    Boolean: Triangulate intersection points

--- apps/boolean.cc
+++ apps/boolean.cc
@@ -11,11 +11,21 @@
 #include <CGAL/AABB_traits.h>
 #include <CGAL/AABB_triangle_primitive.h>
 
-typedef CGAL::Simple_cartesian<double> K;
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Projection_traits_xy_3.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+// typedef CGAL::Simple_cartesian<double> K;
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Projection_traits_xy_3<K> Gt;
+typedef CGAL::Delaunay_triangulation_2<Gt> Delaunay;
+
 typedef K::FT FT;
 typedef K::Ray_3 Ray;
 typedef K::Line_3 Line;
 typedef K::Point_3 Point;
+typedef K::Point_2 Point2;
 typedef K::Triangle_3 Triangle;
 typedef std::list<Triangle>::iterator Iterator;
 typedef CGAL::AABB_triangle_primitive<K, Iterator> Primitive;
@@ -61,15 +71,32 @@ int main(int argc, char **argv)
 
     for (auto const &t2 : mesh_2)
     {
+        auto v1 = t2.vertex(0);
+        auto v2 = t2.vertex(1);
+        auto v3 = t2.vertex(2);
+
+        auto e1 = v2 - v1;
+        auto e2 = v3 - v1;
+
         std::list<Triangle_intersection> intersections;
         tree_1.all_intersections(t2, std::back_inserter(intersections));
+
+        std::list<Point> intersections_2d;
         for (auto const &i : intersections)
         {
             auto p = boost::get<Point>(&(i->first));
             if (p)
             {
-                std::cout << *p << std::endl;
+                auto x_2d = p->x() * e1.x() + p->y() * e1.y();
+                auto y_2d = p->x() * e2.x() + p->y() * e2.y();
+                intersections_2d.push_back({x_2d, y_2d, 0.0});
             }
+        }
+
+        Delaunay dt(intersections_2d.begin(), intersections_2d.end());
+        for (auto it = dt.finite_faces_begin(); it != dt.finite_faces_end(); it++)
+        {
+            std::cout << dt.triangle(it) << std::endl;
         }
     }
 
