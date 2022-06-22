9d30a5a43ae209df2888b2e765345bf2862e5846
Iyad Ahmed <iyadahmed430@gmail.com>
2022-06-22 16:34:58 +0200

    Keep only boolean2.cc as boolean.cc

--- apps/boolean.cc
+++ apps/boolean.cc
@@ -1,9 +1,6 @@
-#include <chrono>
-#include <iostream>
+#include <cstdio>
 #include <vector>
-#include <cmath>
-#include <array>
-#include <fstream>
+#include <iostream>
 
 #include "stl_io.hh"
 
@@ -11,144 +8,137 @@
 #include <CGAL/AABB_tree.h>
 #include <CGAL/AABB_traits.h>
 #include <CGAL/AABB_triangle_primitive.h>
-
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Triangulation_vertex_base_with_info_2.h>
-#include <CGAL/Projection_traits_xy_3.h>
-#include <CGAL/Delaunay_triangulation_2.h>
-
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel_with_kth_root.h>
 #include <CGAL/Triangulation_2.h>
 
-// typedef CGAL::Simple_cartesian<double> K;
+typedef CGAL::Simple_cartesian<double> TreeK;
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Triangulation_vertex_base_with_info_2<int, K> Vb;
+typedef TreeK::Point_3 Point;
+typedef TreeK::Triangle_3 Triangle;
+typedef TreeK::Segment_3 Segment;
+typedef TreeK::Vector_3 Vector;
 
-// typedef CGAL::Projection_traits_xy_3<K> Gt;
-// typedef CGAL::Delaunay_triangulation_2<Gt> Delaunay;
-
-typedef CGAL::Triangulation_data_structure_2<Vb> Tds;
-typedef CGAL::Delaunay_triangulation_2<K, Tds> Delaunay;
-
-typedef K::FT FT;
-typedef K::Ray_3 Ray;
-typedef K::Line_3 Line;
-typedef K::Point_3 Point3;
-typedef K::Point_2 Point2;
-typedef K::Triangle_3 Triangle;
-
-typedef std::list<Triangle>::iterator Iterator;
-typedef CGAL::AABB_triangle_primitive<K, Iterator> Primitive;
-typedef CGAL::AABB_traits<K, Primitive> AABB_triangle_traits;
+typedef std::vector<Triangle>::iterator Iterator;
+typedef CGAL::AABB_triangle_primitive<TreeK, Iterator> Primitive;
+typedef CGAL::AABB_traits<TreeK, Primitive> AABB_triangle_traits;
 typedef CGAL::AABB_tree<AABB_triangle_traits> Tree;
 
+typedef Tree::Primitive_id Primitive_id;
 typedef boost::optional<Tree::Intersection_and_primitive_id<Triangle>::Type> Triangle_intersection;
 
-static std::vector<mp::io::stl::Triangle> load_stl(const char *filepath)
+typedef CGAL::Exact_predicates_inexact_constructions_kernel TriK;
+typedef CGAL::Triangulation_2<TriK> Triangulation;
+typedef Triangulation::Point TriPoint;
+
+static std::vector<Triangle> load_stl(const char *filepath)
 {
     std::vector<mp::io::stl::Triangle> tris;
     mp::io::stl::read_stl(filepath, tris);
-    return tris;
+
+    std::vector<Triangle> out;
+    for (auto const &t : tris)
+    {
+        Point p1(t.v1[0], t.v1[1], t.v1[2]);
+        Point p2(t.v2[0], t.v2[1], t.v2[2]);
+        Point p3(t.v3[0], t.v3[1], t.v3[2]);
+
+        out.push_back({p1, p2, p3});
+    }
+
+    return out;
+}
+
+static std::vector<TriPoint> triangulation_input_from_intersection(const Tree &tree, const Triangle &triangle)
+{
+    std::vector<TriPoint> triangulation_input;
+    std::vector<Triangle_intersection> intersections;
+
+    tree.all_intersections(triangle, std::back_inserter(intersections));
+
+    const Point verts[] = {triangle.vertex(0), triangle.vertex(1), triangle.vertex(2)};
+    auto basis_v1 = verts[1] - verts[0];
+    auto basis_v2 = verts[2] - verts[0];
+
+    // Include original triangle points in triangulation
+    for (int i = 0; i < 3; i++)
+    {
+        auto x = CGAL::scalar_product(verts[i] - CGAL::ORIGIN, basis_v1);
+        auto y = CGAL::scalar_product(verts[i] - CGAL::ORIGIN, basis_v2);
+        triangulation_input.push_back({x, y});
+    }
+
+    // Include intersection result
+    for (auto const &ti : intersections)
+    {
+        if (auto intersection_point = boost::get<Point>(&(ti->first)))
+        {
+            auto x = CGAL::scalar_product((*intersection_point) - CGAL::ORIGIN, basis_v1);
+            auto y = CGAL::scalar_product((*intersection_point) - CGAL::ORIGIN, basis_v2);
+            triangulation_input.push_back({x, y});
+        }
+        else if (auto intersection_segment = boost::get<Segment>(&(ti->first)))
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                auto x = CGAL::scalar_product(intersection_segment->vertex(i) - CGAL::ORIGIN, basis_v1);
+                auto y = CGAL::scalar_product(intersection_segment->vertex(i) - CGAL::ORIGIN, basis_v2);
+                triangulation_input.push_back({x, y});
+            }
+        }
+        else if (auto intersection_triangle = boost::get<Triangle>(&(ti->first)))
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                auto x = CGAL::scalar_product(intersection_triangle->vertex(i) - CGAL::ORIGIN, basis_v1);
+                auto y = CGAL::scalar_product(intersection_triangle->vertex(i) - CGAL::ORIGIN, basis_v2);
+                triangulation_input.push_back({x, y});
+            }
+        }
+        else if (auto intersection_polygon = boost::get<std::vector<Point>>(&(ti->first)))
+        {
+            for (auto const &p : *intersection_polygon)
+            {
+                auto x = CGAL::scalar_product(p - CGAL::ORIGIN, basis_v1);
+                auto y = CGAL::scalar_product(p - CGAL::ORIGIN, basis_v2);
+                triangulation_input.push_back({x, y});
+            }
+        }
+    }
+
+    return triangulation_input;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 3)
+    if (argc != 3)
     {
         puts("Usage: boolean a.stl b.stl");
         return 1;
     }
 
-    std::list<Triangle> mesh_1;
-    for (auto const &t : load_stl(argv[1]))
+    std::cout << "CGAL Version: " << CGAL_VERSION_STR << std::endl;
+
+    auto tri_soup_1 = load_stl(argv[1]);
+    Tree tree_1(tri_soup_1.begin(), tri_soup_1.end());
+
+    auto tri_soup_2 = load_stl(argv[2]);
+
+    for (auto const &tri : tri_soup_2)
     {
-        Point3 p1(t.v1[0], t.v1[1], t.v1[2]);
-        Point3 p2(t.v2[0], t.v2[1], t.v2[2]);
-        Point3 p3(t.v3[0], t.v3[1], t.v3[2]);
-
-        mesh_1.push_back(Triangle(p1, p2, p3));
-    }
-    std::list<Triangle> mesh_2;
-    for (auto const &t : load_stl(argv[2]))
-    {
-        Point3 p1(t.v1[0], t.v1[1], t.v1[2]);
-        Point3 p2(t.v2[0], t.v2[1], t.v2[2]);
-        Point3 p3(t.v3[0], t.v3[1], t.v3[2]);
-
-        mesh_2.push_back(Triangle(p1, p2, p3));
-    }
-
-    Tree tree_1(mesh_1.begin(), mesh_1.end());
-
-    std::vector<mp::io::stl::Triangle> output_tris;
-
-    for (auto const &t2 : mesh_2)
-    {
-        auto v1 = t2.vertex(0);
-        auto v2 = t2.vertex(1);
-        auto v3 = t2.vertex(2);
-
-        auto e1 = v2 - v1;
-        auto e2 = v3 - v1;
-
-        std::vector<Triangle_intersection> intersections;
-        tree_1.all_intersections(t2, std::back_inserter(intersections));
-
-        std::vector<std::pair<Point2, int>> delaunay_input_points;
-        for (int i = 0; i < intersections.size(); i++)
+        auto triangulation_input = triangulation_input_from_intersection(tree_1, tri);
+        Triangulation triangulation(triangulation_input.begin(), triangulation_input.end());
+        std::cout << "Triangulation output: " << std::endl;
+        for (auto it = triangulation.finite_faces_begin(); it != triangulation.finite_faces_end(); it++)
         {
-            auto const &ti = intersections[i];
-            auto p = boost::get<Point3>(&(ti->first));
-            if (p)
+            std::cout << "Triangle: " << std::endl;
+            for (int i = 0; i < 3; i++)
             {
-                auto x_2d = p->x() * e1.x() + p->y() * e1.y() + p->z() * e1.z();
-                auto y_2d = p->x() * e2.x() + p->y() * e2.y() + p->z() * e2.z();
-
-                delaunay_input_points.push_back(std::make_pair(Point2(x_2d, y_2d), i));
-            }
-        }
-
-        Delaunay dt(delaunay_input_points.begin(), delaunay_input_points.end());
-
-        mp::io::stl::Triangle tri_buf{};
-        for (auto it = dt.finite_faces_begin(); it != dt.finite_faces_end(); it++)
-        {
-
-            auto v1_i = it->vertex(0)->info();
-            auto v2_i = it->vertex(1)->info();
-            auto v3_i = it->vertex(2)->info();
-
-            auto p1 = boost::get<Point3>(&(intersections[v1_i]->first));
-            auto p2 = boost::get<Point3>(&(intersections[v2_i]->first));
-            auto p3 = boost::get<Point3>(&(intersections[v3_i]->first));
-
-            // std::cout << "Triangle: " << (*p1) << ", " << (*p2) << ", " << (*p3) << std::endl;
-
-            if (p1 && p2 && p3)
-            {
-                for (int i = 0; i < 3; i++)
-                {
-                    tri_buf.v1[i] = p1->cartesian(i);
-                }
-                for (int i = 0; i < 3; i++)
-                {
-                    tri_buf.v2[i] = p2->cartesian(i);
-                }
-                for (int i = 0; i < 3; i++)
-                {
-                    tri_buf.v3[i] = p3->cartesian(i);
-                }
-                output_tris.push_back(tri_buf);
+                std::cout << triangulation.triangle(it).vertex(i) << std::endl;
             }
         }
     }
-
-    std::cout << "Num out tris = " << output_tris.size() << std::endl;
-
-    write_stl(output_tris, "boolean_output.stl");
-
-    auto tris = load_stl(argv[1]);
-    mp::io::stl::write_stl(tris, "foo.stl");
 
     return 0;
 }
