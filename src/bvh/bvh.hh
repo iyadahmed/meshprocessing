76aaefc7557747bb002ab6222407e839409f390a
Iyad Ahmed <iyadahmed430@gmail.com>
2022-07-06 23:31:15 +0200

    Remove obsolete commented out code

--- src/bvh/bvh.hh
+++ src/bvh/bvh.hh
@@ -1,67 +1,4 @@
-// #include <vector>
-
 #include <cmath>
-
-// #include "vec3.hh"
-
-// // struct float3
-// // {
-// //     float x, y, z;
-// //     float3 cross(const float3 &other) const
-// //     {
-// //         float3 out;
-// //         out.x = y * other.z - z * other.y;
-// //         out.y = z * other.x - x * other.z;
-// //         out.z = x * other.y - y * other.x;
-// //         return out;
-// //     }
-// //     Vec3 operator-(const Vec3 &other) const
-// //     {
-// //         return Vec3(x - other.x, y - other.y, z - other.z);
-// //     }
-// // };
-
-// // typedef float3 Vec3;
-
-// struct BVHTriangle
-// {
-//     Vec3 vertex0, vertex1, vertex2, centroid;
-// };
-
-// struct BVHRay
-// {
-//     Vec3 origin, direction;
-//     float t = 1e30f;
-// };
-// struct BVHNode
-// {
-//     Vec3 aabb_min, aabb_max;
-//     int left_child, right_child;
-//     int first_triangle_index, triangle_count;
-//     bool is_leaf() const { return triangle_count > 0; };
-// };
-
-// class BVH
-// {
-// private:
-//     BVHNode *m_nodes;
-//     int m_used_nodes_num;
-//     // TODO: share triangles between BVH and the outside world (e.g. animation system)
-//     BVHTriangle *m_tris;
-//     int *m_tris_indices;
-//     int m_tris_num;
-
-//     void update_node_bounds(int node_index);
-//     void subdivide(int node_index);
-
-// public:
-//     BVH(int tris_num);
-//     ~BVH();
-
-//     void build();
-//     BVHTriangle &triangle(int index) const;
-//     void intersect_ray(BVHRay &ray, int node_index = 0) const;
-// };
 
 struct float3
 {
@@ -77,10 +14,6 @@ struct float3
     {
         return reinterpret_cast<float *>(this)[index];
     }
-    // const float &operator[](int index) const
-    // {
-    //     return (*this)[index];
-    // }
     void max(const float3 &other)
     {
         x = fmaxf(x, other.x);
