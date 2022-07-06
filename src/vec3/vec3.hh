5cd3e410c92a5e0445625300cd4d19d7faf3dad3
Iyad Ahmed <iyadahmed430@gmail.com>
2022-07-07 00:49:54 +0200

    Update Vec3: refactor and use noexcept

--- src/vec3/vec3.hh
+++ src/vec3/vec3.hh
@@ -2,28 +2,25 @@
  * the main reason to be header only is to allow inlining,
  * which is critical for this data structure, as it is meant to be used in very tight loops (e.g rendering, processing geometry, etc) */
 
-// TODO: refactor into header and source files with move constructor and assignment overriden
+// NOTE: consider refactoring into header and source files with move constructor and assignment overriden to keep some of inlining performance
 
 #pragma once
 
-#include <cstddef>
-#include <ostream>
 #include <cmath>
-#include <stdexcept>
+#include <ostream>
 
-class Vec3
+struct Vec3
 {
-public:
     float x, y, z;
 
-    Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
-    Vec3(const float buf[3])
+    Vec3(float x, float y, float z) noexcept : x(x), y(y), z(z) {}
+    Vec3(const float buf[3]) noexcept
     {
         x = buf[0];
         y = buf[1];
         z = buf[2];
     }
-    Vec3(float value = 0.0f)
+    Vec3(float value = 0.0f) noexcept
     {
         x = y = z = value;
     }
@@ -34,83 +31,93 @@ public:
         return os << "<Vector (" << v.x << ", " << v.y << ", " << v.z << ")>";
     }
 
-    void min(Vec3 &out, const Vec3 &a, const Vec3 &b)
+    static void min(Vec3 &out, const Vec3 &a, const Vec3 &b) noexcept
     {
         out.x = fminf(a.x, b.x);
         out.y = fminf(a.y, b.y);
         out.z = fminf(a.z, b.z);
     }
 
-    void max(Vec3 &out, const Vec3 &a, const Vec3 &b)
+    static void max(Vec3 &out, const Vec3 &a, const Vec3 &b) noexcept
     {
         out.x = fmaxf(a.x, b.x);
         out.y = fmaxf(a.y, b.y);
         out.z = fmaxf(a.z, b.z);
     }
 
-    void min(const Vec3 &other)
+    void min(const Vec3 &other) noexcept
     {
         x = fminf(x, other.x);
         y = fminf(y, other.y);
         z = fminf(z, other.z);
     }
 
-    void max(const Vec3 &other)
+    void max(const Vec3 &other) noexcept
     {
         x = fmaxf(x, other.x);
         y = fmaxf(y, other.y);
         z = fmaxf(z, other.z);
     }
 
-    float dot(const Vec3 &other) const
+    float dot(const Vec3 &other) const noexcept
     {
         return x * other.x + y * other.y + z * other.z;
     }
 
-    Vec3 cross(const Vec3 &other) const
+    Vec3 cross(const Vec3 &other) const noexcept
     {
-        Vec3 out;
-        out.x = y * other.z - z * other.y;
-        out.y = z * other.x - x * other.z;
-        out.z = x * other.y - y * other.x;
-        return out;
+        return {y * other.z - z * other.y,
+                z * other.x - x * other.z,
+                x * other.y - y * other.x};
     }
 
-    float length_squared() const { return x * x + y * y + z * z; }
+    float length_squared() const noexcept
+    {
+        return x * x + y * y + z * z;
+    }
 
-    float length() const { return sqrt(length_squared()); }
+    float length() const noexcept
+    {
+        return sqrt(length_squared());
+    }
 
-    void normalize() { *this /= length(); }
+    void normalize() noexcept
+    {
+        *this /= length();
+    }
 
-    Vec3 normalized() const
+    Vec3 normalized() const noexcept
     {
         float l = length();
-        return Vec3(x / l, y / l, z / l);
+        return {x / l, y / l, z / l};
     }
 
-    Vec3 operator+(const Vec3 &other) const
+    Vec3 operator+(const Vec3 &other) const noexcept
     {
-        return Vec3(x + other.x, y + other.y, z + other.z);
+        return {x + other.x, y + other.y, z + other.z};
     }
 
-    Vec3 operator-(const Vec3 &other) const
+    Vec3 operator-(const Vec3 &other) const noexcept
     {
-        return Vec3(x - other.x, y - other.y, z - other.z);
+        return {x - other.x, y - other.y, z - other.z};
     }
 
-    Vec3 operator-() const { return Vec3(-x, -y, -z); }
-
-    Vec3 operator*(float t) const
+    Vec3 operator-() const noexcept
     {
-        return Vec3(x * t, y * t, z * t);
+        return {-x, -y, -z};
     }
 
-    friend Vec3 operator*(float t, const Vec3 &v)
+    Vec3 operator*(float t) const noexcept
+    {
+        return {x * t, y * t, z * t};
+    }
+
+    friend Vec3 operator*(float t, const Vec3 &v) noexcept
     {
         return v * t;
     }
 
-    Vec3 &operator+=(const Vec3 &other)
+    Vec3 &operator+=(const Vec3 &other) noexcept
     {
         x += other.x;
         y += other.y;
@@ -118,7 +125,7 @@ public:
         return *this;
     }
 
-    Vec3 &operator*=(float t)
+    Vec3 &operator*=(float t) noexcept
     {
         x *= t;
         y *= t;
@@ -126,32 +133,26 @@ public:
         return *this;
     }
 
-    Vec3 &operator/=(float t) { return *this *= 1.0 / t; }
-
-    Vec3 operator/(float t) const { return Vec3(x / t, y / t, z / t); }
-
-    float &operator[](size_t index)
+    Vec3 &operator/=(float t) noexcept
     {
-        if (index == 0)
-        {
-            return x;
-        }
-        else if (index == 1)
-        {
-            return y;
-        }
-        else if (index == 2)
-        {
-            return z;
-        }
-
-        throw std::out_of_range("Index out of range");
+        x /= t;
+        y /= t;
+        z /= t;
+        return *this;
     }
 
-    float operator[](size_t index) const
+    Vec3 operator/(float t) const noexcept
+    {
+        return {x / t, y / t, z / t};
+    }
+
+    float &operator[](size_t index) noexcept
+    {
+        return reinterpret_cast<float *>(this)[index];
+    }
+
+    float operator[](size_t index) const noexcept
     {
         return (*this)[index];
     }
 };
-
-typedef Vec3 Point3;
