f8e75070d358dc63e30d9246dbba5a669b9c2094
Iyad Ahmed <iyadahmed430@gmail.com>
2022-07-06 20:33:41 +0200

    BVH2: implement build

--- apps/bvh2.cc
+++ apps/bvh2.cc
@@ -1,6 +1,22 @@
 #include "bvh.hh"
+#include "random2.hh"
+
+#define N 64
 
 int main()
 {
     TEST();
+
+    auto bvh = BVH(N);
+    for (int i = 0; i < N; i++)
+    {
+        BVHTriangle &tri = bvh.triangle(i);
+        float3 r0{random_float(), random_float(), random_float()};
+        float3 r1{random_float(), random_float(), random_float()};
+        float3 r2{random_float(), random_float(), random_float()};
+        tri.v0 = r0 * 9 - float3(5);
+        tri.v1 = tri.v0 + r1;
+        tri.v2 = tri.v0 + r2;
+    }
+    bvh.build();
 }
--- src/bvh/bvh.hh
+++ src/bvh/bvh.hh
@@ -73,6 +73,26 @@ struct float3
     float3(float x, float y, float z) : x(x), y(y), z(z)
     {
     }
+    float &operator[](int index)
+    {
+        return reinterpret_cast<float *>(this)[index];
+    }
+    // const float &operator[](int index) const
+    // {
+    //     return (*this)[index];
+    // }
+    void max(const float3 &other)
+    {
+        x = fmaxf(x, other.x);
+        y = fmaxf(y, other.y);
+        z = fmaxf(z, other.z);
+    }
+    void min(const float3 &other)
+    {
+        x = fminf(x, other.x);
+        y = fminf(y, other.y);
+        z = fminf(z, other.z);
+    }
     float3 operator-(const float3 &other) const
     {
         return {x - other.x, y - other.y, z - other.z};
@@ -128,12 +148,92 @@ private:
     BVHNode *nodes_;
     BVHTriangle *tris_;
     int tris_num_;
+    int used_nodes_num_;
+    void update_node_bounds(int node_index)
+    {
+        BVHNode &node = nodes_[node_index];
+        node.aabb_max = float3(-1e10);
+        node.aabb_min = float3(1e30);
+
+        for (int i = node.first_tri_index; i < node.tri_count; i++)
+        {
+            const BVHTriangle &leaf_tri = tris_[i];
+            node.aabb_max.max(leaf_tri.v0);
+            node.aabb_max.max(leaf_tri.v1);
+            node.aabb_max.max(leaf_tri.v2);
+
+            node.aabb_min.min(leaf_tri.v0);
+            node.aabb_min.min(leaf_tri.v1);
+            node.aabb_min.min(leaf_tri.v2);
+        }
+    }
+    void subdivide(int node_index)
+    {
+        BVHNode &node = nodes_[node_index];
+        if (node.tri_count <= 2)
+        {
+            return;
+        }
+        float3 extent = node.aabb_max - node.aabb_min;
+
+        int axis = 0;
+        if (extent.y > extent.x)
+        {
+            axis = 1;
+        }
+        if (extent.z > extent[axis])
+        {
+            axis = 2;
+        }
+        float split_pos = node.aabb_min[axis] + extent[axis] * .5f;
+
+        // In-place parition
+        int i = node.first_tri_index;
+        int j = node.tri_count + i - 1;
+        while (i <= j)
+        {
+            if (tris_[i].centroid[axis] < split_pos)
+            {
+                i++;
+            }
+            else
+            {
+                std::swap(tris_[i], tris_[j--]);
+            }
+        }
+
+        // Abort split if one of the sides is empty
+        int left_count = i - node.first_tri_index;
+        if (left_count == 0 || left_count == node.tri_count)
+        {
+            return;
+        }
+
+        // Create child nodes
+        int left_child_index = used_nodes_num_++;
+        int right_child_index = used_nodes_num_++;
+
+        nodes_[left_child_index].first_tri_index = node.first_tri_index;
+        nodes_[left_child_index].tri_count = left_count;
+        nodes_[right_child_index].first_tri_index = i;
+        nodes_[right_child_index].tri_count = node.tri_count - left_count;
+
+        node.first_tri_index = left_child_index;
+        node.tri_count = 0;
+        update_node_bounds(left_child_index);
+        update_node_bounds(right_child_index);
+
+        // Recurse
+        subdivide(left_child_index);
+        subdivide(right_child_index);
+    }
 
 public:
     BVH(int tris_num) : tris_num_(tris_num)
     {
         nodes_ = new BVHNode[tris_num * 2]{};
         tris_ = new BVHTriangle[tris_num]{};
+        used_nodes_num_ = 1;
     }
     ~BVH()
     {
@@ -160,6 +260,9 @@ public:
         BVHNode &root = nodes_[0];
         root.first_tri_index = root.left_child = root.right_child = 0;
         root.tri_count = tris_num_;
+
+        update_node_bounds(0);
+        subdivide(0);
     }
 };
 
