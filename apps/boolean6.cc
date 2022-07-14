36f00acee4ec8a929ce499624f4c8dbe289f49d9
Iyad Ahmed <iyadahmed430@gmail.com>
2022-07-14 16:28:14 +0200

    Boolean6: calc_bounds function

--- apps/boolean6.cc
+++ apps/boolean6.cc
@@ -63,30 +63,22 @@ struct CellTriangleIndexPair
     int cell_index, triangle_index;
 };
 
-// Finds index by searching for cell index with binary search
-// -1 means it was not found
-int index_of_cell_index(const CellTriangleIndexPair *arr, int n, int cell_index)
+struct CTriangle
 {
-    int start = 0;
-    int end = n - 1;
-    int mid;
-    while (start != end)
+    int start_x, start_y, start_z;
+    int end_x, end_y, end_z;
+};
+
+static inline void calc_bounds(const stl::Triangle &t, Vec3 &min, Vec3 &max)
+{
+    Vec3 *verts = (Vec3 *)t.verts;
+    min = verts[0];
+    max = verts[0];
+    for (int i = 1; i < 3; i++)
     {
-        mid = (start + end) / 2;
-        if (cell_index == arr[mid].cell_index)
-        {
-            return mid;
-        }
-        else if (cell_index > arr[mid].cell_index)
-        {
-            start = mid + 1;
-        }
-        else
-        {
-            end = mid - 1;
-        }
+        min.min(verts[i]);
+        max.max(verts[i]);
     }
-    return -1;
 }
 
 int main(int argc, char *argv[])
@@ -104,38 +96,27 @@ int main(int argc, char *argv[])
     stl::read_stl(filepath_1, tri_soup);
     stl::read_stl(filepath_2, tri_soup);
 
-    Vec3 bb_min(INFINITY);
-    Vec3 bb_max(-INFINITY);
-    Vec3 bb_dims_avg(0.0f);
-
-    Vec3 t_bb_min(INFINITY);
-    Vec3 t_bb_max(-INFINITY);
+    Vec3 bb_min(INFINITY), bb_max(-INFINITY);
 
     Timer timer;
     for (auto const &tri : tri_soup)
     {
-        t_bb_min = INFINITY;
-        t_bb_max = -INFINITY;
         for (int i = 0; i < 3; i++)
         {
             bb_min.min(tri.verts[i]);
             bb_max.max(tri.verts[i]);
-
-            t_bb_min.min(tri.verts[i]);
-            t_bb_max.max(tri.verts[i]);
         }
-        bb_dims_avg += (t_bb_max - t_bb_min) / tri_soup.size();
+        // todo: use std::accumulate to calc avg bounding box in parallel
+        // bb_dims_avg += (t_bb_max - t_bb_min) / tri_soup.size();
     }
     timer.tock("First Pass");
 
-    // TODO: dynamic grid step
+    // TODO: dynamic grid step?
     float grid_step = .3;
     Vec3 bb_dims = bb_max - bb_min;
     int num_x = std::ceil(bb_dims.x / grid_step);
     int num_y = std::ceil(bb_dims.y / grid_step);
     int num_z = std::ceil(bb_dims.z / grid_step);
-
-    // std::vector<std::vector<int>> cells(num_x * num_y * num_z);
 
     std::vector<CellTriangleIndexPair> cells(tri_soup.size());
 
@@ -143,13 +124,8 @@ int main(int argc, char *argv[])
     for (int i = 0; i < tri_soup.size(); i++)
     {
         const auto &t = tri_soup[i];
-        t_bb_min = INFINITY;
-        t_bb_max = -INFINITY;
-        for (int i = 0; i < 3; i++)
-        {
-            t_bb_min.min(t.verts[i]);
-            t_bb_max.max(t.verts[i]);
-        }
+        Vec3 t_bb_min, t_bb_max;
+        calc_bounds(t, t_bb_min, t_bb_max);
         // TODO: store triangle start and end x, y, z
         for (int x = std::ceil(t_bb_min.x / grid_step); x < std::ceil(t_bb_max.x / grid_step); x++)
         {
@@ -179,13 +155,9 @@ int main(int argc, char *argv[])
     for (int i = 0; i < tri_soup.size(); i++)
     {
         const auto &t = tri_soup[i];
-        t_bb_min = INFINITY;
-        t_bb_max = -INFINITY;
-        for (int i = 0; i < 3; i++)
-        {
-            t_bb_min.min(t.verts[i]);
-            t_bb_max.max(t.verts[i]);
-        }
+        Vec3 t_bb_min, t_bb_max;
+        calc_bounds(t, t_bb_min, t_bb_max);
+        // FIXME:: use floor for start x, y, z and ceil for end
         for (int x = std::ceil(t_bb_min.x / grid_step); x < std::ceil(t_bb_max.x / grid_step); x++)
         {
             for (int y = std::ceil(t_bb_min.y / grid_step); y < std::ceil(t_bb_max.y / grid_step); y++)
@@ -244,7 +216,7 @@ int main(int argc, char *argv[])
 
     // TODO: intersect edges instead
 
-    std::cout << "Avg. Triangle BB = " << bb_dims_avg << std::endl;
+    // std::cout << "Avg. Triangle BB = " << bb_dims_avg << std::endl;
     std::cout << "Mesh Bounding Box: Max(" << bb_max << "), Min(" << bb_min << ")" << std::endl;
     std::cout << "Cells Size " << cells.size() << std::endl;
     std::cout << "Intersection Size " << interescetions.size() << std::endl;
