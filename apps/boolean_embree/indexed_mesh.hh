760a7fdd077861c6686440870022612651d1e73b
Iyad Ahmed <iyadahmed430@gmail.com>
2022-07-16 08:09:35 +0200

    IndexedMesh

--- apps/boolean_embree/indexed_mesh.hh
+++ apps/boolean_embree/indexed_mesh.hh
@@ -1,8 +1,10 @@
 #pragma once
 
-#include <unordered_set>
+#include <unordered_map>
 #include <utility> // for std::swap
 #include <vector>
+
+#include "vec3.hh"
 
 struct IndexedTriangle
 {
@@ -71,20 +73,10 @@ struct std::hash<IndexedEdge>
     }
 };
 
-struct IndexedVertex
-{
-    float x, y, z;
-};
-
-bool operator==(const IndexedVertex &lhs, const IndexedVertex &rhs)
-{
-    return (lhs.x == rhs.x) && (lhs.y == rhs.y) && (lhs.z == rhs.z);
-}
-
 template <>
-struct std::hash<IndexedVertex>
+struct std::hash<Vec3>
 {
-    std::size_t operator()(IndexedVertex const &value) const noexcept
+    std::size_t operator()(Vec3 const &value) const noexcept
     {
         // https://stackoverflow.com/a/16794345/8094047
         size_t h1 = std::hash<float>()(value.x);
@@ -94,10 +86,72 @@ struct std::hash<IndexedVertex>
     }
 };
 
-struct IndexedMesh
+class IndexedMesh
 {
-    std::unordered_set<IndexedVertex> verts_;
-    std::vector<IndexedVertex> verts;
+public:
+    std::unordered_map<Vec3, size_t> verts_;
+    std::vector<Vec3> verts;
+
+    std::unordered_map<IndexedEdge, size_t> edges_;
     std::vector<IndexedEdge> edges;
+
+    std::unordered_map<IndexedTriangle, size_t> tris_;
     std::vector<IndexedTriangle> tris;
+
+    int add_vertex(Vec3 pos)
+    {
+        auto it = verts_.find(pos);
+        if (it != verts_.end())
+        {
+            return it->second;
+        }
+        size_t new_index = verts.size();
+        verts_.insert({pos, new_index});
+        verts.push_back(pos);
+        return new_index;
+    };
+
+    int add_edge(int v1, int v2)
+    {
+        auto it = edges_.find({v1, v2});
+        if (it != edges_.end())
+        {
+            return it->second;
+        }
+        size_t new_index = edges.size();
+        edges_.insert({{v1, v2}, new_index});
+        edges.push_back({v1, v2});
+        return new_index;
+    }
+
+    int add_edge(Vec3 a, Vec3 b)
+    {
+        int v1 = add_vertex(a);
+        int v2 = add_vertex(b);
+        add_edge(v1, v2);
+    }
+
+    int add_triangle(int v1, int v2, int v3)
+    {
+        add_edge(v1, v2);
+        add_edge(v2, v3);
+        add_edge(v3, v1);
+
+        auto it = tris_.find({v1, v2, v3});
+        if (it != tris_.end())
+        {
+            return it->second;
+        }
+        it->second = tris.size();
+        tris.push_back({v1, v2, v3});
+        return it->second;
+    }
+
+    int add_triangle(Vec3 a, Vec3 b, Vec3 c)
+    {
+        int v1 = add_vertex(a);
+        int v2 = add_vertex(b);
+        int v3 = add_vertex(c);
+        return add_triangle(v1, v2, v3);
+    }
 };
