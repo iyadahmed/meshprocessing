14f3fa8866f3073e6aff709a92936d6f8030015b
Iyad Ahmed <iyadahmed430@gmail.com>
2022-07-23 15:40:40 +0200

    Remove obsolete comments

--- apps/random2.hh
+++ apps/random2.hh
@@ -2,10 +2,7 @@
 
 #include <random>
 
-std::random_device
-    rd; // Will be used to obtain a seed for the random number engine
-std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+std::random_device rd;
+std::mt19937 gen(rd());
 std::uniform_real_distribution<> dis(0.0, 1.0);
-
-/* Uniform random float in range 0.0 1.0 */
 float random_float() { return dis(gen); }
