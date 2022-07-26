b8538890821625574aa305d54e36c4f62d341f1c
Iyad Ahmed <iyadahmed430@gmail.com>
2022-07-26 22:28:54 +0200

    BSP: Insert unique points

--- apps/bsp.cc
+++ apps/bsp.cc
@@ -8,6 +8,6 @@ int main()
 	for (int i = 0; i < 10; i++)
 		for (int j = 0; j < 10; j++)
 			for (int k = 0; k < 10; k++)
-				tree.insert(i, j, k);
+				tree.insert(0, 0, 0);
 	std::cout << tree.num << std::endl;
 }
--- apps/bsp.hh
+++ apps/bsp.hh
@@ -5,6 +5,7 @@ struct BSPNode {
   float nx = 0.0f, ny = 0.0f,
         nz = 0.0f; // Plane normal (no need to be normalized)
   BSPNode *left = nullptr, *right = nullptr;
+  size_t index; // TODO: we can probably get rid of this
   bool is_leaf() const { return (left == nullptr) && (right == nullptr); }
 };
 
@@ -21,19 +22,21 @@ struct BSPTree {
     num = 0;
   }
 
-  ~BSPTree()
-  {
-	// FIXME: avoid double free when copying object
-	delete [] nodes;
+  ~BSPTree() {
+    // FIXME: avoid double free when copying object
+    delete[] nodes;
   }
 
   BSPNode *new_node() {
     if (num > cap)
       throw;
-    return nodes + (num++);
+    BSPNode *node = nodes + num;
+    node->index = num;
+    num++;
+    return node;
   }
 
-  void insert(float x, float y, float z) {
+  size_t insert(float x, float y, float z, float merge_distance=0.0001f) {
     if (num == 0) {
       auto node = this->new_node();
       node->x = x;
@@ -43,37 +46,46 @@ struct BSPTree {
       node->ny = 0.0f;
       node->nz = 0.0f;
       node->left = node->right = nullptr;
-    } else {
-      BSPNode *node = nodes;
-      // Find empty leaf to insert new node
-      while (node->left && node->right) {
-        float dx = x - node->x;
-        float dy = y - node->y;
-        float dz = z - node->z;
-        float d = dx * node->nx + dy * node->ny + dz * node->nz;
-        if (d > 0.0f) {
-          node = node->left;
-        } else {
-          node = node->right;
-        }
-      }
-      BSPNode *new_node = this->new_node();
-      new_node->x = x;
-      new_node->y = y;
-      new_node->z = z;
-      new_node->nx = node->x - x;
-      new_node->ny = node->y - y;
-      new_node->nz = node->z - z;
-      new_node->left = node->right = nullptr;
+      return 0;
+    }
+
+    // Find empty leaf to insert new node
+    BSPNode *node = nodes;
+    while (node->left && node->right) {
       float dx = x - node->x;
       float dy = y - node->y;
       float dz = z - node->z;
       float d = dx * node->nx + dy * node->ny + dz * node->nz;
       if (d > 0.0f) {
-        node->left = new_node;
+        node = node->left;
       } else {
-        node->right = new_node;
+        node = node->right;
       }
     }
+
+    float dx = x - node->x;
+    float dy = y - node->y;
+    float dz = z - node->z;
+    if ((dx * dx + dy * dy + dz * dz) < merge_distance) {
+      return node->index;
+    }
+
+    BSPNode *new_node = this->new_node();
+    new_node->x = x;
+    new_node->y = y;
+    new_node->z = z;
+    new_node->nx = node->x - x;
+    new_node->ny = node->y - y;
+    new_node->nz = node->z - z;
+    new_node->left = node->right = nullptr;
+
+    float d = dx * node->nx + dy * node->ny + dz * node->nz;
+    if (d > 0.0f) {
+      node->left = new_node;
+    } else {
+      node->right = new_node;
+    }
+
+	return new_node->index;
   }
 };
